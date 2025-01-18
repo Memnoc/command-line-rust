54d0a0e824e1469e9b84cb610981f4bbfa3649a3
Memnoc <smartdroidesign@gmail.com>
2025-01-18 16:28:12 +0000

    testing and learning about exit values

--- exercises/hello_world/tests/cli.rs
+++ exercises/hello_world/tests/cli.rs
@@ -1,4 +1,4 @@
-use assert_cmd::Command;
+use assert_cmd::{cargo::CommandCargoExt, Command};
 
 #[test]
 fn works() {}
@@ -6,5 +6,17 @@ fn works() {}
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello_world").unwrap();
+    cmd.assert().success().stdout("Hello World!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
 }
