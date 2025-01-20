ee4c80f55a22af734166e16b4f961332125aa071
Memnoc <smartdroidesign@gmail.com>
2025-01-20 18:33:28 +0000

    clap sample implementation

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,22 @@
-use clap::Command;
+use clap::{Arg, Command};
 fn main() {
-    let _matches = Command::new("echor")
+    let matches = Command::new("echor")
         .author("Memnoc, <memnochmod@gmail.com>")
         .version("1.0.0")
         .about("Rust echor")
-        .help_template("{bin} {version}\nBy: {author}\n{about}\n\n{usage}\n\nOptions:\n{options}") // Custom template
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
+    println!("{:#?}", matches);
 }
