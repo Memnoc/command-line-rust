e03b799b5de05a18c279e678ae79da8fc2e1aa9c
Memnoc <smartdroidesign@gmail.com>
2025-01-20 07:01:33 +0000

    clap init and Command instead of App

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,16 +1,9 @@
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-struct Args {
-    name: String,
-    count: u8,
-}
-
+use clap::Command;
 fn main() {
-    println!("{:?}", std::env::args());
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+    let _matches = Command::new("echor")
+        .author("Memnoc, <memnochmod@gmail.com>")
+        .version("1.0.0")
+        .about("Rust echor")
+        .help_template("{bin} {version}\nBy: {author}\n{about}\n\n{usage}\n\nOptions:\n{options}") // Custom template
+        .get_matches();
 }
