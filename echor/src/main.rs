3172497a0ddec2e696043a8742d95847e0661186
Memnoc <smartdroidesign@gmail.com>
2025-01-20 06:35:22 +0000

    clap examle

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    name: String,
+    count: u8,
+}
+
 fn main() {
     println!("{:?}", std::env::args());
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
