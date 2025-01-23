ab930e407476be7adc9a9fe2169cdc39bc014ed4
Memnoc <smartdroidesign@gmail.com>
2025-01-23 06:54:06 +0000

    functionality reached

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,22 +1,25 @@
-use clap::{Arg, Command};
+use clap::{App, Arg};
 fn main() {
-    let matches = Command::new("echor")
+    let matches = App::new("echor")
         .author("Memnoc, <memnochmod@gmail.com>")
         .version("1.0.0")
         .about("Rust echor")
         .arg(
-            Arg::new("text")
+            Arg::with_name("text")
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
-                .num_args(1..),
+                .min_values(1),
         )
         .arg(
-            Arg::new("omit_newline")
-                .short('n')
+            Arg::with_name("omit_newline")
+                .short("n")
                 .help("Do not print newline")
-                .action(clap::ArgAction::SetTrue),
+                .takes_value(false),
         )
         .get_matches();
-    println!("{:#?}", matches);
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
